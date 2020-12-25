5b746e7d9fcbea595b4199a05dbf39f03656a6ea
Juan Luis Millan Hidalgo <juanluismillanhidalgo@gmail.com>
2020-12-25 22:37:59 +0100

    foto

--- traspaso.sh
+++ traspaso.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo "Bienvenido al actualizador de git de Juan Luis Millán." 
+echo "Bienvenido al actualizador de git de Juan Luis Millán."
 echo "Introduce los cambios realizados(commit):"; read input
 
 while [$input = ""]
 do
- echo "Opción no valida, comenta algo." 
+ echo "Opción no valida, comenta algo."
  echo "Introduce los cambios realizados(commit):"; read input
 done
 
 #Actualizamos los ficheros modificados. La opción incremental solo generará un documento o página si se modifica,es decir, solo afectara a las páginas nuevas o modificadas
-bundle exec jekyll build 
-#--incremental
+bundle exec jekyll build --incremental
 
 #Copiamos los html creados en nuestro _site y los movemos a nuestro repositorio que esta enlazado a netlify
 cp -r /home/juanlu/GitHub/Blog-QuestTIC/_site/* /home/juanlu/GitHub/QuestTIC/
@@ -22,5 +21,5 @@ cd /home/juanlu/GitHub/QuestTIC/
 #Agreamos los cambios realizados con git,hacemos el commit y lo subimos con un push.
 git add *
 
-git commit -am "$input" 
+git commit -am "$input"
 git push
