212651eb4bcc3fe9dc7d2408ed1418cb6affa019
Juan Luis Millan Hidalgo <juanluismillanhidalgo@gmail.com>
2020-12-25 21:52:15 +0100

    millan

--- traspaso.sh
+++ traspaso.sh
@@ -3,6 +3,12 @@
 echo "Bienvenido al actualizador de git de Juan Luis Millán." 
 echo "Introduce los cambios realizados(commit):"; read input
 
+while [$input = ""]
+do
+ echo "Opción no valida, comenta algo." 
+ echo "Introduce los cambios realizados(commit):"; read input
+done
+
 #Actualizamos los ficheros modificados. La opción incremental solo generará un documento o página si se modifica,es decir, solo afectara a las páginas nuevas o modificadas
 bundle exec jekyll build --incremental
 
@@ -14,12 +20,6 @@ cd /home/juanlu/GitHub/QuestTIC/
 
 #Agreamos los cambios realizados con git,hacemos el commit y lo subimos con un push.
 git add *
-
-while [$input = ""]
-do
- echo "Opción no valida, comenta algo." 
- echo "Introduce los cambios realizados(commit):"; read input
-done
 
 git commit -am "$input" 
 git push
